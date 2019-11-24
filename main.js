2d9daf6f46d6cae14515f45dd660877f8a39bf57
Hemant Patidar <hemant.eblue@gmail.com>
2019-11-24 16:52:57 +0530

    responce

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const channelForm = document.getElementById('channel-form');
 const channelInput = document.getElementById('channel-input');
 const videoContainer = document.getElementById('video-container');
 
-const defaultChannel = 'techguyweb';
+const defaultChannel = '';
 
 // Form submit and change channel
 channelForm.addEventListener('submit', e => {
@@ -80,49 +80,41 @@ function showChannelData(data) {
 
 // Get channel from API
 function getChannel(channel) {
-  let responseGender = await gapi.client.youtubeAnalytics.reports.query({
-    "ids": "channel==MINE",
-    "startDate": "2015-01-01",
-    "endDate": getDate(),
-    "dimensions": "gender",
-    "metrics": "viewerPercentage"
-});
-console.log(responseGender)
-  // gapi.client.youtube.channels
-  //   .list({
-  //     part: 'snippet,contentDetails,statistics',
-  //     forUsername: channel
-  //   })
-  //   .then(response => {
-  //     console.log(response);
-  //     const channel = response.result.items[0];
+  gapi.client.youtube.channels
+    .list({
+      part: 'snippet,contentDetails,statistics',
+      forUsername: channel
+    })
+    .then(response => {
+      console.log(response);
+      const channel = response.result.items[0];
 
-  //     const output = `
-  //       <ul class="collection">
-  //         <li class="collection-item">Title: ${channel.snippet.title}</li>
-  //         <li class="collection-item">ID: ${channel.id}</li>
-  //         <li class="collection-item">Subscribers: ${numberWithCommas(
-  //           channel.statistics.subscriberCount
-  //         )}</li>
-  //         <li class="collection-item">Views: ${numberWithCommas(
-  //           channel.statistics.viewCount
-  //         )}</li>
-  //         <li class="collection-item">Videos: ${numberWithCommas(
-  //           channel.statistics.videoCount
-  //         )}</li>
-  //       </ul>
-  //       <p>${channel.snippet.description}</p>
-  //       <hr>
-  //       <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${
-  //         channel.snippet.customUrl
-  //       }">Visit Channel</a>
-  //     `;
-  //     showChannelData(output);
+      const output = `
+        <ul class="collection">
+          <li class="collection-item">Title: ${channel.snippet.title}</li>
+          <li class="collection-item">ID: ${channel.id}</li>
+          <li class="collection-item">Subscribers: ${numberWithCommas(
+            channel.statistics.subscriberCount
+          )}</li>
+          <li class="collection-item">Views: ${numberWithCommas(
+            channel.statistics.viewCount
+          )}</li>
+          <li class="collection-item">Videos: ${numberWithCommas(
+            channel.statistics.videoCount
+          )}</li>
+        </ul>
+        <p>${channel.snippet.description}</p>
+        <hr>
+        <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${
+          channel.snippet.customUrl
+        }">Visit Channel</a>
+      `;
+      showChannelData(output);
 
-  //     const playlistId = channel.contentDetails.relatedPlaylists.uploads;
-  //     requestVideoPlaylist(playlistId);
-  //   })
-  //   .catch(err => alert('No Channel By That Name'));
+      const playlistId = channel.contentDetails.relatedPlaylists.uploads;
+      requestVideoPlaylist(playlistId);
+    })
+    .catch(err => alert('No Channel By That Name'));
 }
 
 // Add commas to number
